8b1daa03137d04c8abc2ac7722366acf18ae28c2
Shriya Tyagi <shriya243@gmail.com>
2023-12-16 15:38:21 +0530

    feat: set up morpho blue contracts

---
+++ contracts/mainnet/connectors/morpho-blue/events.sol
@@ -0,0 +1,106 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+pragma experimental ABIEncoderV2;
+import { MarketParams } from "./interfaces/IMorpho.sol";
+
+contract Events {
+	event LogSupplyAssets(
+		MarketParams marketParams,
+		uint256 assets,
+		uint256 shares,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogSupplyOnBehalf(
+		MarketParams marketParams,
+		uint256 assets,
+		uint256 shares,
+		address onBehalf,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogSupplyCollateral(
+		MarketParams marketParams,
+		uint256 assets,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogSupplyCollateralOnBehalf(
+		MarketParams marketParams,
+		uint256 assets,
+		address onBehalf,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogBorrow(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 shares,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogBorrowOnBehalf(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 shares,
+		address onBehalf,
+		address receiver,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogWithdraw(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 shares,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogWithdrawOnBehalf(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 shares,
+		address onBehalf,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogWithdrawCollateral(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogWithdrawCollateralOnBehalf(
+		MarketParams marketParams,
+		uint256 amounts,
+		address onBehalf,
+		address receiver,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogRepay(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 shares,
+		uint256 getId,
+		uint256 setId
+	);
+
+	event LogRepayOnBehalf(
+		MarketParams marketParams,
+		uint256 amounts,
+		uint256 shares,
+		address onBehalf,
+		uint256 getId,
+		uint256 setId
+	);
+}
