75568b48c2d699b5ee6f0992da851102302170ca
Shriya Tyagi <shriya243@gmail.com>
2024-04-15 13:34:15 +0400

    feat: prettier

--- contracts/mainnet/connectors/weeth/events.sol
+++ contracts/mainnet/connectors/weeth/events.sol
@@ -2,6 +2,16 @@
 pragma solidity 0.8.19;
 
 contract Events {
-    event LogDeposit(uint256 eETHAmount, uint256 weETHAmount, uint256 getId, uint256 setId);
-    event LogWithdraw(uint256 weETHAmount, uint256 eETHAmount, uint256 getId, uint256 setId);
+    event LogDeposit(
+        uint256 eETHAmount,
+        uint256 weETHAmount,
+        uint256 getId,
+        uint256 setId
+    );
+    event LogWithdraw(
+        uint256 weETHAmount,
+        uint256 eETHAmount,
+        uint256 getId,
+        uint256 setId
+    );
 }
