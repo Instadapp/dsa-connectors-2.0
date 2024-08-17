2e0555469ab11780586fc268772842b3bcf054c5
Samarendra Gouda <73637398+SamarendraGouda@users.noreply.github.com>
2024-08-17 18:51:46 +0530

    Update contracts/arbitrum/connectors/fluid-merkle-claim/main.sol
    Co-authored-by: Shriya Tyagi <47134275+shriyatyagii@users.noreply.github.com>

--- contracts/arbitrum/connectors/fluid-merkle-claim/main.sol
+++ contracts/arbitrum/connectors/fluid-merkle-claim/main.sol
@@ -34,7 +34,7 @@ abstract contract FluidMerkleClaim is Basic, Events {
         uint256 rewardsClaimed_ = TOKEN.balanceOf(address(this)) - rewardsBeforeBal_;
         setUint(setId_, rewardsClaimed_);
 
-        _eventName = "LogClaim(uint256,address,uint256,bytes32[],uint256,uint256)";
+        _eventName = "LogClaim(uint256,bytes32,uint256,bytes32[],uint256,uint256)";
         _eventParam = abi.encode(
             cumulativeAmount_,
             positionId_,
