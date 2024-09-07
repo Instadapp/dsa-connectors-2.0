c1b07c69fad50fbbb9ea83797334a1050b8c6bd2
Samarendra Gouda <samarendragouda@gmail.com>
2024-09-07 12:46:14 +0530

    chore: add support for latest solidity version & upgrade solmate

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -71,6 +71,15 @@ const config: any = {
   solidity: {
     compilers: [
       {
+        version: "0.8.27",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
         version: "0.8.19",
         settings: {
           optimizer: {
