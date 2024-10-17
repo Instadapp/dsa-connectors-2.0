8eb4c1f06c23d7f449acadb04e7edffa6eb53b2a
Samarendra Gouda <samarendragouda@gmail.com>
2024-10-18 03:02:12 +0530

    fix: connector name

--- contracts/base/connectors/fluid/main.sol
+++ contracts/base/connectors/fluid/main.sol
@@ -299,5 +299,5 @@ abstract contract FluidConnector is Events, Basic {
 }
 
 contract ConnectV2FluidBase is FluidConnector {
-    string public constant name = "Fluid-v1.0";
+    string public constant name = "Fluid-v1.1";
 }
