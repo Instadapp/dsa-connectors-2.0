63147dc53b153a571edb6a541f94d6ef5976713f
Samarendra Gouda <samarendragouda@gmail.com>
2025-03-18 03:37:48 +0530

    feat(base): add fluid-dex connectors

---
+++ contracts/base/connectors/fluid-dex/main.sol
@@ -0,0 +1,90 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+/**
+ * @title Fluid Dex.
+ * @dev Dex.
+ */
+
+import {Events} from "./events.sol";
+import {IFluidDex} from "./interface.sol";
+import {TokenInterface} from "../../common/interfaces.sol";
+import {Basic} from "../../common/basic.sol";
+
+abstract contract FluidDex is Basic, Events {
+    /**
+     * @dev Returns Eth address
+     */
+    function getEthAddr() internal pure returns (address) {
+        return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    }
+
+    /**
+     * @dev Deposits in Fluid Dex
+     * @param dex_ Fluid dex address
+     * @param token0Amt_ The amount of token0 to deposit
+     * @param token1Amt_ The amount of token1 to deposit
+     * @param minSharesAmt_ The minimum amount of shares the user expects to receive
+     */
+    function deposit(
+        address dex_,
+        uint256 token0Amt_,
+        uint256 token1Amt_,
+        uint256 minSharesAmt_
+    )
+        external
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        IFluidDex.ConstantViews memory constantsView_ = IFluidDex(dex_)
+            .constantsView();
+
+        uint256 ethAmt_;
+        bool token0IsMax_ = token0Amt_ == type(uint256).max;
+        bool token1IsMax_ = token1Amt_ == type(uint256).max;
+
+        if (constantsView_.token0 == getEthAddr()) {
+            ethAmt_ = token0IsMax_ ? address(this).balance : token0Amt_;
+        } else {
+            if (token0IsMax_) {
+                token0Amt_ = TokenInterface(constantsView_.token0).balanceOf(
+                    address(this)
+                );
+            }
+
+            approve(TokenInterface(constantsView_.token0), dex_, token0Amt_);
+        }
+
+        if (constantsView_.token1 == getEthAddr()) {
+            ethAmt_ = token1IsMax_ ? address(this).balance : token1Amt_;
+        } else {
+            if (token1IsMax_) {
+                token1Amt_ = TokenInterface(constantsView_.token1).balanceOf(
+                    address(this)
+                );
+            }
+
+            approve(TokenInterface(constantsView_.token1), dex_, token1Amt_);
+        }
+
+        // Deposit in Fluid Dex
+        IFluidDex(dex_).deposit{value: ethAmt_}(
+            token0Amt_,
+            token1Amt_,
+            minSharesAmt_,
+            false
+        );
+
+        _eventName = "LogFluidDexDeposit(address,uint256,uint256,uint256)";
+        _eventParam = abi.encode(
+            dex_,
+            token0Amt_,
+            token1Amt_,
+            minSharesAmt_
+        );
+    }
+}
+
+contract ConnectV2FluidDexBase is FluidDex {
+    string public constant name = "Fluid-dex-v1.0";
+}
