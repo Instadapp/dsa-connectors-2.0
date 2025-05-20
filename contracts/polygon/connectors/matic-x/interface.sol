6733b2e1c39194ce8365d3bb59f69155bda874f1
Samarendra Gouda <samarendragouda@gmail.com>
2025-05-20 20:07:36 +0530

    fix: add setId and refactor

--- contracts/polygon/connectors/matic-x/interface.sol
+++ contracts/polygon/connectors/matic-x/interface.sol
@@ -1,7 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-
 interface IStaderChildPool {
     function swapMaticForMaticXViaInstantPool() external payable;
+
+    function convertMaticToMaticX(
+        uint256 _balance
+    ) external view returns (uint256, uint256, uint256);
 }
