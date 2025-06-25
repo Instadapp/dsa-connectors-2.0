c3225c1fef415174be5583fb9daf013d3a56f804
Samarendra Gouda <samarendragouda@gmail.com>
2025-06-25 19:08:27 +0530

    fix: add validations

--- contracts/mainnet/connectors/etherfi-mint/main.sol
+++ contracts/mainnet/connectors/etherfi-mint/main.sol
@@ -6,6 +6,8 @@ import {Events} from "./events.sol";
 import {TokenInterface} from "../../common/interfaces.sol";
 
 contract EtherfiMintConnector is Helpers, Events {
+    error EtherfiMintConnector__AmountReceivedIsLess();
+
     function mintEethFromStEth(
         uint256 depositAmount_
     )
@@ -13,6 +15,10 @@ contract EtherfiMintConnector is Helpers, Events {
         payable
         returns (string memory _eventName, bytes memory _eventParam)
     {
+        if (depositAmount_ == type(uint256).max) {
+            depositAmount_ = address(this).balance;
+        }
+
         // Approve the Etherfi Vampire contract to spend the specified amount of stETH
         TokenInterface(STETH_ADDRESS).approve(
             address(ETHERFI_VAMPIRE_CONTRACT),
@@ -25,6 +31,12 @@ contract EtherfiMintConnector is Helpers, Events {
             depositAmount_,
             REFERRAL_ADDRESS
         );
+
+        // In reality the difference between depositAmount_ and eEthAmount_ is 1 Wei,
+        // but keeping a buffer of 1 Gwei
+        if (eEthAmount_ < (depositAmount_ - 1e9)) {
+            revert EtherfiMintConnector__AmountReceivedIsLess();
+        }
 
         _eventName = "LogMintEtherfi(address,uint256)";
         _eventParam = abi.encode(depositAmount_, eEthAmount_);
